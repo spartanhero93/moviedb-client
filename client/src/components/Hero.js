5b71f7b3a8661d5bd10d64045b3298fa2bdb1289
spartanhero93 <spartanhero93@gmail.com>
2024-04-09 23:26:44 -0400

    removed the styled component from Hero.js

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import styled from 'styled-components'
 
-const Hero = () => {
-  return (
-    <div>
-      <Wrapper>
-        <h1>Welcome to the MovieDB app</h1>
-        <p>Browse all kinds of media using MDAPI</p>
-      </Wrapper>
-    </div>
-  )
-}
-
-export const HeroRoute = () => <Route exact path='/' component={Hero} />
-
-const Wrapper = styled.div`
-  height: 50vh;
+const styles = {
+  container: `height: 50vh;
   width: 100%;
   border: 2px solid;
   text-align: center;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-`
+  justify-content: center;`
+}
+
+const Hero = () => {
+  return (
+    <div className={styles.container}>
+        <h1>Welcome to the MovieDB app</h1>
+        <p>Browse all kinds of media using MDAPI</p>
+    </div>
+  )
+}
+
+export const HeroRoute = () => <Route exact path='/' component={Hero} />
